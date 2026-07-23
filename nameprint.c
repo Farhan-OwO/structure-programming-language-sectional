6debf72305edb494278aae455c5c7f561bfbcb3e
Farhan-OwO <152715895+Farhan-OwO@users.noreply.github.com>
2026-07-23 18:13:11 +0600

    Add files via upload
    HW1

---
+++ nameprint.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+
+void main(){
+
+    printf("   --Farhan bin asad-- \n");
+
+    printf("    **************** \n    **************** \n    *** \n    *** \n    ********** \n    ********** \n    *** \n    *** \n    *** \n    *** \n    *** \n    *** ");
+
+}
